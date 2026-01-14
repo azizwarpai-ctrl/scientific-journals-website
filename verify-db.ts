7c68a34dc2ceb4adf47752dc7dd8627c8f3552a0
mohammed3200 <wwyuu799@gmail.com>
2026-01-14 20:23:07 +0200

    feat(prisma): add initial Prisma configuration and database models
    - Introduced a new Prisma configuration file to manage database connections and migrations.
    - Updated the database connection logic in 'verify-db.ts' for improved error handling and logging.
    - Expanded the Prisma schema with new models for Journal, Submission, Review, PublishedArticle, SystemSetting, Message, and FAQ, establishing relationships and indexes for better data management.
    - Enhanced the seeding script to utilize the PrismaMariaDb adapter for MySQL/MariaDB, ensuring compatibility with the new database setup.

--- verify-db.ts
+++ verify-db.ts
@@ -1,16 +1,15 @@
-
-import { prisma } from "./lib/prisma";
+import { prisma } from './lib/db/config'
 
 async function main() {
-    try {
-        const result = await prisma.$queryRaw`SELECT 1 as result`;
-        console.log("Connection successful:", result);
-    } catch (error) {
-        console.error("Connection failed:", error);
-        process.exit(1);
-    } finally {
-        await prisma.$disconnect();
-    }
+  try {
+    const result = await prisma.$queryRaw`SELECT 1 as result`
+    console.log('Connection successful:', result , "✅")
+  } catch (error) {
+    console.error('Connection failed:', error,"❌")
+    process.exit(1)
+  } finally {
+    await prisma.$disconnect()
+  }
 }
 
-main();
+main()
